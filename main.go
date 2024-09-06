3b0d11d0054e57abe9ff6eee61056f544567da7c
Nuratin <gusnievnuratin2017@gmail.com>
2024-09-06 12:30:02 +0300

    second commit

--- main.go
+++ main.go
@@ -19,7 +19,11 @@ func main() {
 		// right-click your code in the editor and select the <b>Debug</b> option.
 		fmt.Println("i =", 100/i)
 	}
+	{
+	}
 }
 
 //TIP See GoLand help at <a href="https://www.jetbrains.com/help/go/">jetbrains.com/help/go/</a>.
 // Also, you can try interactive lessons for GoLand by selecting 'Help | Learn IDE Features' from the main menu.
+
+//some changes
